999389a2ab8264df6470c12bcb00ebc0fc89f70a
Deniz Tutuncu <denizsafaktutuncu@gmail.com>
2021-09-08 14:03:16 -0700

    Sut `print` number Zero

--- FizzBuzzApp/FizzBuzzAppTests/FizzBuzzAppTests.swift
+++ FizzBuzzApp/FizzBuzzAppTests/FizzBuzzAppTests.swift
@@ -8,10 +8,19 @@
 import XCTest
 
 class FizzBuzzEngine {
- 
+    func print(_ number: Int) -> String {
+        return "0"
+    }
 }
 
 class FizzBuzzAppTests: XCTestCase {
+    
+    func test_print_numberZero() {
+        let sut = makeSUT()
+        let number = sut.print(0)
+        
+        XCTAssertEqual(number, "0")
+    }
     
     private func makeSUT() -> FizzBuzzEngine {
         let sut = FizzBuzzEngine()
